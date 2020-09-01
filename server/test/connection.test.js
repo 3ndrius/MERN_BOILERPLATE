5a6fefc68210ec6922987d06edc45a3f00515df4
3ndrius <andrzejo1991@gmail.com>
2020-09-01 23:58:07 +0200

    Add connection to db test / hook before

--- server/test/connection.test.js
+++ server/test/connection.test.js
@@ -1,1 +1,20 @@
 
+// hooks 
+require('../config/database')
+
+// before test check connection 
+describe('hooks before', () => {
+
+    before((done) => {
+        mongoose.connect(dbConnectionURL.LOCALURL, options);
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'Mongodb Connection Error:' + dbConnectionURL.LOCALURL));
+        db.once('open', () => {
+
+            console.log('###### ======== Mongodb Connection Successful ========= ########');
+            done()
+        });
+        done()
+    })
+
+})
